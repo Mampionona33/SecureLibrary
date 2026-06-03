6afb2bab53a19bc24a54afd7d1a9360f2072ad46
Mampionona33 <ramamps33@gmail.com>
2026-06-03 14:46:24 +0300

    ajout auto remove file after post delete file to backend

--- Backend/library/views.py
+++ Backend/library/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets, permissions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
-
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from .models import Book
 from .serializers import BookSerializer
+import os
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -26,3 +28,21 @@ class BookViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         print("RAW DATA:", request.body)
         return super().create(request, *args, **kwargs)
+
+
+# ✅ Signal défini en dehors de la classe
+@receiver(post_delete, sender=Book)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Supprime automatiquement le PDF et l'image du dossier media 
+    lorsque l'objet Book est supprimé.
+    """
+    # Supprimer le PDF
+    if instance.pdf_file and instance.pdf_file.path:
+        if os.path.isfile(instance.pdf_file.path):
+            os.remove(instance.pdf_file.path)
+
+    # Supprimer l'image
+    if instance.cover_image and instance.cover_image.path:
+        if os.path.isfile(instance.cover_image.path):
+            os.remove(instance.cover_image.path)
