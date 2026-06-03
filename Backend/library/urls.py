8d1cea0406ae6862c2d6692ff439dd535291300a
Mampionona33 <ramamps33@gmail.com>
2026-06-03 07:43:05 +0300

    correction route add book

--- Backend/library/urls.py
+++ Backend/library/urls.py
@@ -3,8 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
 router = DefaultRouter()
-router.register(r'library', BookViewSet, basename='book')
+router.register(r'', BookViewSet, basename='book')
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = router.urls
