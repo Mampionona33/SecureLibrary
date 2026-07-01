e6a82825059befbff61bbe1278391b365de6af8b
Mampionona33 <ramamps33@gmail.com>
2026-07-01 17:10:41 +0300

    replace expo by react native cli

--- Frontend/src/app/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Slot } from "expo-router";
-
-export default function RootLayout() {
-  const isLoggedIn = false; // ton état auth
-  return <Slot initialRouteName={isLoggedIn ? "(drawer)" : "(auth)"} />;
-}
