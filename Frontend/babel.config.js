04d51263ac292109e94bdf8c1a1b8900bbbe71ef
Mampionona33 <ramamps33@gmail.com>
2026-07-03 20:44:16 +0300

    using alias

--- Frontend/babel.config.js
+++ Frontend/babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          "@components": "./src/components",
+          "@screens": "./src/screens",
+          "@hooks": "./src/hooks",
+          "@assets": "./src/assets",
+          "@navigation": "./src/navigation",
+          "@utils": "./src/utils",
+          "@constants": "./src/constants",
+          "@services": "./src/services",
+          "@store": "./src/store",
+          "@context": "./src/context",
+          "@api": "./src/api"
+        }
+      }
+    ]
+  ]
 };
+
--- Frontend/src/navigation/AppNavigator.tsx
+++ Frontend/src/navigation/AppNavigator.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LoginScreen from '../screens/Login/LoginScreen';
+import LoginScreen from '@screens/Login';
+// import LoginScreen from '@screens/Login/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import { RootStackParamList } from './types';
 
