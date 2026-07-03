2cd83641a6cc738c47426c023d318f33af73a2aa
Mampionona33 <ramamps33@gmail.com>
2026-07-03 22:30:16 +0300

    refactorisation home

--- Frontend/src/screens/Home/index.tsx
+++ Frontend/src/screens/Home/index.tsx
@@ -1,48 +1,33 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '@navigation/types';
-
-// 1. On importe le hook du contexte
-import { useAuth } from '@context/AuthContext';
-
-type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
-
-const HomeScreen = ({ navigation }: Props) => {
-  // 2. On récupère la fonction de déconnexion globale
-  const { logout } = useAuth();
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Page d'Accueil</Text>
-      <Text style={styles.subtitle}>Bienvenue !</Text>
-      
-      <Button 
-        title="Se déconnecter" 
-        color="red"
-        // 3. On appelle simplement la fonction du contexte
-        onPress={logout} 
-      />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 18,
-    marginBottom: 30,
-  }
-});
-
+// src/screens/Home/index.tsx
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '@navigation/types';
+
+// 1. On importe le hook du contexte
+import { useAuth } from '@context/AuthContext';
+
+// 2. On importe nos styles isolés
+import { styles } from './styles';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const HomeScreen = ({ navigation }: Props) => {
+  // On récupère la fonction de déconnexion globale
+  const { logout } = useAuth();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Page d'Accueil</Text>
+      <Text style={styles.subtitle}>Bienvenue !</Text>
+      
+      <Button 
+        title="Se déconnecter" 
+        color="red"
+        onPress={logout} 
+      />
+    </View>
+  );
+};
+
 export default HomeScreen;
