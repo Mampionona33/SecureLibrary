d6bde7bd68ccc2a21262cc069cca84ad2a181757
Mampionona33 <ramamps33@gmail.com>
2026-07-16 17:51:23 +0300

    change access token validity to 5min

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT configuration
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
