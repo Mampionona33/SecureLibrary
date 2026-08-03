8fe2a6d65b265ff81c0093a59c12acf16bee88de
Mampionona33 <ramamps33@gmail.com>
2026-08-03 04:20:18 +0300

    update api url front

--- Frontend/src/api/config.ts
+++ Frontend/src/api/config.ts
@@ -1,6 +1,6 @@
 export const MAIN_API_BASE_URL = __DEV__
   ? 'http://192.168.10.102:8000/api'  // ✅ Votre IP locale pour le développement
-  : ' https://shaggy-flies-tickle.loca.lt';  // ✅ URL Localtunnel pour la release
+  : ' https://honest-camels-stand.loca.lt/api';  // ✅ URL Localtunnel pour la release
 
 export const API_TIMEOUT = 300000;
 export const API_MAX_CONTENT_LENGTH = 50 * 1024 * 1024;
